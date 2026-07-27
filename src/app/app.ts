abfb0b40d36ec6773dcfcccd957abf98a2eeb804
Erik Cleyverton <erik.c.correia@aluno.senai.br>
2026-07-27 20:48:42 -0300

    Ajusta componentes e finaliza calculadora

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalculadoraComponent } from './calculadora/calculadora';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalculadoraComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('calculadora');
+export class AppComponent {
+  title = 'calculadora';
 }
---
+++ src/app/calculadora/calculadora.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-calculadora',
+  imports: [FormsModule],
+  templateUrl: './calculadora.html',
+  styleUrl: './calculadora.css'
+})
+export class CalculadoraComponent {
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  somar() {
+    this.resultado = this.numero1 + this.numero2;
+  }
+
+  subtrair() {
+    this.resultado = this.numero1 - this.numero2;
+  }
+
+  multiplicar() {
+    this.resultado = this.numero1 * this.numero2;
+  }
+
+  dividir() {
+    this.resultado = this.numero2 !== 0 ? this.numero1 / this.numero2 : 0;
+  }
+}
